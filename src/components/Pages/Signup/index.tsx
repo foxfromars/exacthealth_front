78a4224219a29fccd057588d904cfe74a1501893
luisfeliperaposobatista <luisfeliperaposobatista@gmail.com>
2023-02-14 22:17:49 -0300

    state in form

--- src/components/Pages/Signup/index.tsx
+++ src/components/Pages/Signup/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import {
   TextField,
   Button,
@@ -9,7 +10,8 @@ import {
 } from "./styles";
 
 export default function Signup() {
-  const [values, setValues] = useState({});
+  const [values, setValues] = useState<any>({});
+
   const handleChange = (e: any) => {
     const { name, value } = e.target;
     setValues({
@@ -17,28 +19,47 @@ export default function Signup() {
       [name]: value
     })
   }
+
+  const handleSubmit = () => {
+    const SERVER_URL: string = import.meta.env.VITE_SERVER_URL;
+    axios.post(`${SERVER_URL}/user`,)
+  }
+
   return (
     <AccountForm>
       <Text>Sign Up to acess our services</Text>
       <TextField
         label="Name"
         name="name"
+        onChange={handleChange}
         value={values.name || ""}
       />
       <TextField
         label="Email"
         name="email"
+        onChange={handleChange}
+        value={values.email || ""}
       />
       <TextField
         label="Username"
         name="username"
+        onChange={handleChange}
+        value={values.username || ""}
       />
       <TextField
         label="Password"
         type="password"
         name="password"
+        onChange={handleChange}
+        value={values.password || ""}
       />
-      <Button variant="contained">Sign Up</Button>
+
+      <Button
+        variant="contained"
+        onClick={handleSubmit}
+      >
+        Sign Up
+      </Button>
     </AccountForm>
   );
 }
